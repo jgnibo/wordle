f29ffff1ab26e2d3667e21d85f37acbc30e3918f
Brendan Berkman <bberkman20@riverdale.edu>
2022-02-09 20:10:59 -0500

    wordle

--- WordleBot.py
+++ WordleBot.py
@@ -6,12 +6,21 @@ class WordleBot():
         content = f.read()
 
         self.words = content.splitlines()
+        self.wordsDict = {}
         self.charFrequencyMap = self.generateCharFrequencyMap()
         self.wordScores = []
 
         self.results = []
         self.greyLetters = {}
-        print(self.charFrequencyMap)
+        self.yellows = []
+        self.yellowCount = {}
+        
+        self.alpha = "abcdefghijklmnopqrstuvwxyz"
+        self.yellowCount = {}
+        
+
+
+        # print(self.charFrequencyMap)
 
     def generateCharFrequencyMap(self):
         charFrequencyMap = {}
@@ -30,6 +39,15 @@ class WordleBot():
     def playWordle(self, result):
         print("test")
 
+    def genLastYellowCount(self):
+        for char in self.alpha:
+            self.yellowCount[char] = 0
+
+        for item in self.yellows:
+            if(item[1] == "yellow"):
+                self.yellowCount.update({item[0]: self.yellowCount[item[0]]+1})
+
+
     def calculateWordScore(self, word):
         score = 0
 
@@ -46,13 +64,62 @@ class WordleBot():
         return score/len(word)
 
     def generateWordScore(self):
+        self.wordScores.clear()
+        self.removeWords()
         for word in self.words:
             self.wordScores.append((word, self.calculateWordScore(word)))
 
         return self.wordScores
+    
+    def checkGreys(self, word):
+        for character in word:
+            if(character in self.greyLetters):
+                return(False)
+        
+        return(True)
+
+    def checkYellows(self, word):
+        i = 0
+
+        if(len(self.yellows)==0):
+            return(True)
+
+        self.genLastYellowCount()
+        yellowChecker = {}
+        
+        for char in self.alpha:
+            yellowChecker[char] = 0
+
+        for character in word:
+            if((self.yellows[i][1] == "yellow") and (self.yellows[i][0] == character)):
+                return(False)
+            elif(self.yellowCount[character]!=0):
+                yellowChecker.update({character: yellowChecker[character] + 1})
+
+            i += 1
+        
+
+        for char in yellowChecker:
+            if yellowChecker[char] != self.yellowCount[char]:
+                return(False)
+            
+        return(True)
+    
+    def updateYellows(self, lst):
+        self.yellows = lst
+    
+    def removeWords(self):
+        wordLst = []
+        for word in self.words:
+            if(self.checkYellows(word)==False or self.checkGreys(word)==False):
+                wordLst.append(word)
+        
+        for word in wordLst:
+            self.words.remove(word)
 
     def findNextWord(self):
         top = 0
+
         for index, wordTuple in enumerate(self.wordScores):
             if wordTuple[1] > self.wordScores[top][1]:
                 top = index
@@ -60,10 +127,17 @@ class WordleBot():
         return self.wordScores[top]
 
 
-
 def main():
     wordle_bot = WordleBot("words.txt")
-    #print(wordle_bot.getCharFrequencyMap())
+    print(wordle_bot.checkYellows("ioeoi"))
+    wordle_bot.generateWordScore()
+    print(wordle_bot.findNextWord())
+    wordle_bot.updateYellows([('a', 'yellow'), ('r', 'yellow'), ('o', 'grey'), ('s', 'yellow'), ('e', 'grey')])
+    wordle_bot.greyLetters["o"] = 1
+    wordle_bot.generateWordScore()
+    print(wordle_bot.findNextWord())
+    wordle_bot.updateYellows([('l', 'grey'), ('a', 'yellow'), ('s', 'yellow'), ('e', 'yellow'), ('r', 'yellow')])
+    wordle_bot.greyLetters["l"] = 1
     wordle_bot.generateWordScore()
     print(wordle_bot.findNextWord())
 
